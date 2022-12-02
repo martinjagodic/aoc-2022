5038dd05e2530673f20e2c72545018d995337d69
martinjagodic <jagodicmartin1@gmail.com>
2022-12-02 11:17:48 +0100

    add day 2 solution, reorganize

---
+++ day1/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+
+const elves = fs
+  .readFileSync(path.resolve(__dirname, "./input.txt"))
+  .toString()
+  .split("\n\n")
+
+const cals = []
+
+elves.forEach(e => {
+  const elf = e.split("\n").map(el => parseInt(el, 10))
+  cals.push(elf.reduce((acc, val) => acc + val))
+})
+
+const sorted = cals.sort((a, b) => b - a)
+
+console.log("star 1 result: ", sorted[0])
+console.log("star 2 result: ", sorted.slice(0,3).reduce((acc, val) => acc + val))
---
+++ day2/index.js
@@ -0,0 +1,64 @@
+const fs = require('fs')
+const path = require('path')
+
+const points = {
+  rock: 1,
+  paper: 2,
+  scissors: 3,
+  win: 6,
+  draw: 3,
+  lose: 0,
+}
+
+var lines = fs
+  .readFileSync(path.resolve(__dirname, "./input.txt"))
+  .toString()
+  .replaceAll("A", "rock").replaceAll("B", "paper").replaceAll("C", "scissors")
+  
+  /* --- star 1 --- */
+  // .replaceAll("Y", "paper").replaceAll("X", "rock").replaceAll("Z", "scissors")
+
+  /* --- star 2 --- */
+  .replaceAll("Y", "draw").replaceAll("X", "lose").replaceAll("Z", "win")
+  .split("\n")
+
+let totalPoints = 0
+
+lines.forEach(line => {
+  const arr = line.split(" ")
+  const hisMove = arr[0]
+  const outcome = arr[1]
+
+  /* --- star 1 --- */
+  // let outcome = "lose"
+  // if (hisMove === myMove) {
+  //   outcome = "draw"
+  // } else if (
+  //   (hisMove === "rock" && myMove === "paper") ||
+  //   (hisMove === "paper" && myMove === "scissors") ||
+  //   (hisMove === "scissors" && myMove === "rock")
+  // ) {
+  //   outcome = "win"
+  // }
+
+  /* --- star 2 --- */
+  let myMove = "rock"
+  if (
+    (outcome === "win" && hisMove === "rock") ||
+    (outcome === "draw" && hisMove === "paper") ||
+    (outcome === "lose" && hisMove === "scissors")
+  ) {
+    myMove = "paper"
+  } else if (
+    (outcome === "win" && hisMove === "paper") ||
+    (outcome === "draw" && hisMove === "scissors") ||
+    (outcome === "lose" && hisMove === "rock")
+  ) {
+    myMove = "scissors"
+  }
+
+  const result = points[myMove] + points[outcome]
+  totalPoints += result
+})
+
+console.log("total points: ", totalPoints)
